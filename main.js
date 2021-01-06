26defd36503d3389837ab2d0c62302c6c8321ec4
Maciej <maciejmad16@gmail.com>
2021-01-06 11:09:47 +0000

    habit of creating function for easy creating elements

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 const container = document.querySelector(".container");
+
+function create(tagName, attributeType, aValue) {
+    const tag = document.createElement(tagName);
+    tag.setAttribute(attributeType, aValue);
+    return tag;
+}
