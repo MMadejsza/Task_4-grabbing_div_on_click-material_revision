312c8e02c364c9abe4119b8375fd4baa07831edf
Maciej <maciejmad16@gmail.com>
2021-01-06 11:20:00 +0000

    Start task structure in JS

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 const container = document.querySelector(".container");
+const square = create("div", "class", "square");
+container.appendChild(square);
+
+
+let squareX = 150;
+let squareY = 100;
+square.style.left = `${squareX}px`;
+square.style.top = `${squareY}px`;
+
+const mouseDown = () => {
+
+}
+const mouseMove = () => {
+
+}
+const mouseUp = () => {
+
+}
+
+
+
+
 
 function create(tagName, attributeType, aValue) {
     const tag = document.createElement(tagName);
