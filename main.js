f8a8745bce5a000ac964ae1fc99be968d3c67b95
Maciej <maciejmad16@gmail.com>
2021-01-06 11:44:31 +0000

    Proper moving added - still prior to proper cursor position

--- main.js
+++ main.js
@@ -7,18 +7,26 @@ let squareX = 150;
 let squareY = 100;
 square.style.left = `${squareX}px`;
 square.style.top = `${squareY}px`;
+let drawBrakePoint = false;
 
 square.addEventListener("mousedown", () => {
     console.log('mouseDown');
     square.classList.toggle("squareClicked");
+    drawBrakePoint = !drawBrakePoint;
 })
-square.addEventListener("mousemove", () => {
+square.addEventListener("mousemove", (event) => {
     console.log('mousemove');
-
+    if (drawBrakePoint) {
+        squareX = event.clientX;
+        squareY = event.clientY;
+        square.style.left = `${squareX - 50}px`;
+        square.style.top = `${squareY - 50}px`;
+    }
 })
 square.addEventListener("mouseup", () => {
     console.log('mouseup');
     square.classList.toggle("squareClicked");
+    drawBrakePoint = !drawBrakePoint;
 })
 
 function create(tagName, attributeType, aValue) {
